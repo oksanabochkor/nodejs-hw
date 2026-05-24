b1871ad51a000b58dd9237b598bcb9af78cc9544
Oksana Bochkor <oksanabockor785@gmail.com>
2026-05-24 15:28:19 +0300

    fix controller import

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,5 +1,53 @@
 import { Note } from '../models/note.js';
 
+export const getAllNotes = async (req, res) => {
+  const {
+    page = 1,
+    perPage = 10,
+    tag,
+    search,
+  } = req.query;
+
+  const skip = (page - 1) * perPage;
+
+  const filter = {};
+
+  if (tag) {
+    filter.tag = tag;
+  }
+
+  if (search) {
+    filter.$or = [
+      {
+        title: {
+          $regex: search,
+          $options: 'i',
+        },
+      },
+      {
+        content: {
+          $regex: search,
+          $options: 'i',
+        },
+      },
+    ];
+  }
+
+  const totalNotes = await Note.countDocuments(filter);
+
+  const notes = await Note.find(filter)
+    .skip(skip)
+    .limit(Number(perPage));
+
+  res.status(200).json({
+    page: Number(page),
+    perPage: Number(perPage),
+    totalNotes,
+    totalPages: Math.ceil(totalNotes / perPage),
+    notes,
+  });
+};
+
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
 
